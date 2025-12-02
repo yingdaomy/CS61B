7144c7ff07d7a70ebc13b88d6a90de7c9d11a878
t0mato <yingdaomy37@gmail.com>
2025-12-02 20:12:28 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,19 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** If n is even, the next number is n/2.
+     *  If n is odd, the next number is 3 * n + 1.
+     *  If n equals 1, the sequence is over.
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
         } else {
-            return n * 2;
+            if (n % 2 == 0) {
+                return n / 2;
+            } else {
+                return n * 3 + 1;
+            }
         }
     }
 
