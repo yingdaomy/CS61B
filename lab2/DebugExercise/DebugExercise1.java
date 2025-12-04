97d4ca14bb0cf6fac3cd6612cc9c4e67769b0a82
t0mato <yingdaomy37@gmail.com>
2025-12-04 18:43:40 +0800

    done with lab2

--- lab2/DebugExercise/DebugExercise1.java
+++ lab2/DebugExercise/DebugExercise1.java
@@ -6,9 +6,9 @@ package DebugExercise;
  */
 public class DebugExercise1 {
     public static int divideThenRound(int top, int bottom) {
-        int quotient = top / bottom;
-        int result = Math.round(quotient);
-        return result;
+        double quotient = (double)top / bottom;
+        long result = Math.round(quotient);
+        return (int)result;
     }
 
     public static void main(String[] args) {
